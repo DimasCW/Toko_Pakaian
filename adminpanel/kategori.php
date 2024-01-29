3f940c68b14c07a1e7db3fb9ec5cc91d9d7cee55
DimasCW <cdimas292@gmail.com>
2024-01-29 09:04:43 +0700

    Menambahkan Data

--- adminpanel/kategori.php
+++ adminpanel/kategori.php
@@ -17,6 +17,7 @@
     <title>Kategori</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    
 </head>
 <style>
         .no-decoration{
@@ -40,6 +41,7 @@
                 </ol>
             </nav>
 
+            <!-- Menambahkan Data Kategori -->
             <div class="my-5  col-12 col-md-6">
                 <h3>Tambah Kategori</h3>
                 <form action="" method="post">
@@ -66,7 +68,19 @@
                             <?php
                         }
                         else{
+                            $querySimpan = mysqli_query($con, "INSERT INTO kategori (nama) VALUES ('$kategori')");
+                            if($querySimpan){
+                                ?>
+                                    <div class="alert alert-primary mt-3" role="alert">
+                                        Kategori Tersimpan
+                                    </div>
+                                    <meta http-equiv="refresh" content="1; url=kategori.php">
+                                <?php
 
+                            }
+                            else{
+                                echo mysqli_error($con);
+                            }
                         }
                     }
                 ?>
@@ -101,6 +115,11 @@
                                             <tr>
                                                 <td><?php echo $jumlah?></td>
                                                 <td><?php echo $data['nama']?></td>
+                                                <td>
+                                                    <a href="kategori-detail.php?<?php echo $data['id']?>" class="btn btn-info">
+                                                    <i class="fas fa-search"></i>
+                                                </a>
+                                                </td>
                                             </tr>
                                         <?php
                                         $jumlah++;
